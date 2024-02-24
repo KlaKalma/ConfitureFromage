fa1a577414bc46bc58e2beeac661de4edda5fd87
Clarence LINDEN <clarence.linden@epfl.ch>
2024-02-24 15:10:49 +0100

    spawner

---
+++ DwarfGuardian/Assets/PlayerSpawner.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    // grid manager reference
+    public GridManager gridManager;
+    public GameObject gnomePrefab;
+
+    private float _width, _height;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the width and height inside of the grid manager
+        _width = gridManager._width;
+        _height = gridManager._height;
+
+        // get a random number form 2 to 5
+        int random = Random.Range(2, 6);
+
+        for (int i = 0; i < random; i++)
+        {
+            SpawnPlayer();
+        }
+        
+
+    }
+
+    void SpawnPlayer()
+    {
+        // get a random number from 1 to 3
+        int randomSide = 1;
+
+
+        // 1 = left 2 = bottom 3 = right
+        Vector3 spawnPosition = Vector3.zero;
+        if (randomSide == 1) // left
+        {
+            spawnPosition = new Vector3(- _width / 2, Random.Range(-_height / 2, _height / 2), 0);
+        }
+        else if (randomSide == 2) // bottom
+        {
+            spawnPosition = new Vector3(Random.Range(-_width / 2, _width / 2), -_height / 2, 0);
+        }
+        else if (randomSide == 3) // right
+        {
+            spawnPosition = new Vector3(_width / 2, Random.Range(-_height / 2, _height / 2), 0);
+        }
+
+        // spawn the player
+        GameObject newGnome = Instantiate(gnomePrefab, spawnPosition, Quaternion.identity);
+
+        // make the gnome a child of the player spawner
+        newGnome.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
